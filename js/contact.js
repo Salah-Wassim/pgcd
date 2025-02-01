1660d6c2e8a6b152dc32036db28bbd6d4b9f384b
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-02-01 23:57:56 +0100

    Improved form. Integration of business logic. CSS style integration

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,38 @@
-window.onload = function() {
-    document.getElementById('contactForm').addEventListener('submit', function(e) {
-        e.preventDefault();
-        emailjs.sendForm('service_uhgn27n', 'contactForm', this)
-            .then(() => {
-                console.log('SUCCESS!');
-                document.querySelector(".confirmation").textContent =
-                    "Votre message a bien été envoyé !";
-            }, (error) => {
-                console.log('FAILED...', error);
-                document.querySelector(".confirmation").textContent =
-                    "Une erreur est survenue. Veuillez réessayer.";
-            });
-    });
-}
+const form = document.getElementById('contactForm');
+const inputs = form.querySelectorAll('.input-form-contact');
+const btnFormContactSubmit = document.querySelector('.btn-form-contact');
+const messageInfo = document.querySelector(".message-info-form-contact");
+const emailInput = document.querySelector(".input-form-contact-email");
+const nameInput = document.querySelector(".input-form-contact-name");
+
+btnFormContactSubmit.addEventListener('submit', function(e){
+    e.preventDefault();
+    let inputsFilled = false;
+
+    inputs.forEach(input => {
+        if(!input.checkValidity()){
+            messageInfo.innerHTML = "Veuillez renseignez tous les champs";
+            input.style.border = "1px solid red";
+            inputsFilled = false;
+        }
+        else{
+            messageInfo.innerHTML = "";
+            input.style.border = "1px solid #000";
+            inputsFilled = true;
+        }
+    })
+
+    if(inputsFilled){
+        let regexEmail = /^[\p{L}0-9._%+-]+@[\p{L}0-9.-]+\.[\p{L}]{2,}$/u
+        if(!regexEmail.test(emailInput.value.trim())){
+            messageInfo.innerHTML = "Le format est incorrecte";
+            emailInput.style.border = "1px solid red";
+        }
+    
+        let regexName = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/
+        if(regexName.test(nameInput.value.trim())){
+            messageInfo.innerHTML = "Le format est incorrecte";
+            nameInput.style.border = "1px solid red";
+        }
+    }
+})

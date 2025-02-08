f7e3815f6766c747eeec61f16acd475bed2619b0
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-02-08 05:31:22 +0100

    Integration of a delete button for the input factor field. Update of the form.css file. Integration of business logic.

--- js/inputs.js
+++ js/inputs.js
@@ -1,7 +1,31 @@
 const btnNumbers = document.querySelector(".clear-button-numbers");
+const btnFactors = document.querySelector(".clear-button-factors");
+const checkboxSwitch = document.querySelector('.switch-checkbox');
 const numbers = document.querySelector(".input-numbers");
+const factors = document.querySelector(".input-factor")
 
 btnNumbers.addEventListener("click", function(){
     numbers.value="";
     numbers.focus();
+})
+
+if(factors.type === "hidden"){
+    btnFactors.classList.remove("clear-button")
+    btnFactors.classList.add("active-factors")
+}
+
+checkboxSwitch.addEventListener('change', function(){
+    if(checkboxSwitch.checked){
+        btnFactors.classList.remove("active-factors")
+        btnFactors.classList.add("clear-button")
+    }
+    else{
+        btnFactors.classList.remove("clear-button")
+        btnFactors.classList.add("active-factors")
+    }
+})
+
+btnFactors.addEventListener("click", function(){
+    factors.value="";
+    factors.focus();
 })

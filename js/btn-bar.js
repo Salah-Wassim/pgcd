dd2de3796ff5b262104a49aafb646ae21faa8616
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-02-11 12:26:00 +0100

    Change the background colour of the PGCD and PPCM buttons when clicked.

--- js/btn-bar.js
+++ js/btn-bar.js
@@ -3,12 +3,18 @@ const btnBarPpcm = document.querySelector(".btn-ppcm-bar");
 const pgcdInformations = document.querySelector(".pgcd-informations");
 const ppcmInformations = document.querySelector(".ppcm-informations");
 
+btnBarPgcd.style.background = "#fff";
+
 btnBarPgcd.addEventListener('click', function(){
     pgcdInformations.classList.remove("hide-pgcd-informations");
     ppcmInformations.classList.add("hide-ppcm-informations");
+    btnBarPpcm.style.background = "#ddd";
+    btnBarPgcd.style.background = "#fff";
 })
 
 btnBarPpcm.addEventListener('click', function(){
     pgcdInformations.classList.add("hide-pgcd-informations");
     ppcmInformations.classList.remove("hide-ppcm-informations");
+    btnBarPgcd.style.background = "#ddd";
+    btnBarPpcm.style.background = "#fff";
 })

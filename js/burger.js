2389effa457def0f27367e9d74644161b75031b2
Salah-Wassim <salahwassim.arfae1@gmail.com>
2025-02-15 02:35:34 +0100

    Integration of the history section. Updated HTML architecture of index.html page. Creation of index.css and history.css pages. Integration of css style. Integration of business logic.

--- js/burger.js
+++ js/burger.js
@@ -12,3 +12,31 @@ burger.addEventListener("click", function(){
         burger.classList.add("fa-bars")
     }
 })
+
+const historyContent = document.querySelector(".history-section")
+const indexContent = document.querySelector(".index-content")
+const btnSideBarHeader = document.querySelector(".btn-sidebar-header")
+const btnSideBarClose = document.querySelector(".btn-sidebar-close");
+
+function toggleSidebar(show) {
+    historyContent.classList.toggle("sidebar-show", show);
+    indexContent.style.marginLeft = "0";
+    btnSideBarHeader.style.display = show ? "none" : "block";
+    historyContent.style.width = show ? "200px" : "0";
+}
+
+btnSideBarHeader.addEventListener("click", () => toggleSidebar(true));
+btnSideBarClose.addEventListener("click", () => toggleSidebar(false));
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 940) {
+        indexContent.style.marginLeft = "200px";
+        btnSideBarHeader.style.display = "none";
+        historyContent.style.width = "200px";
+    }
+    else{
+        indexContent.style.marginLeft = "0";
+        btnSideBarHeader.style.display = "block";
+        historyContent.style.width = "0";
+    }
+});
